1647fb900b27508aee74c915571a4c17bfbde2e0
diego <dieg.avendano@vallegrande.edu.pe>
2022-12-16 19:21:50 -0500

    formulario

--- registro.js
+++ registro.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/api/pedido/";
+const url = "http://34.224.143.210:3000/api/pedido/";
 let resultados = '';
 const formArticulo = document.querySelector("form");
 const userped = document.getElementById("USERPED");
